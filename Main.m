464bb188bb4e818c65b97ea8a9437b27eb90d337
David-Badiane <55662004+David-Badiane@users.noreply.github.com>
2021-04-10 22:09:47 +0200

    funzionalizzando

---
+++ Main.m
@@ -0,0 +1,102 @@
+close all
+clearvars
+clc
+addpath 'CSV\pressure' 
+addpath 'CSV\velocity' 
+addpath 'functions'
+
+%% read the csv
+velocityFileName = 'vel_1.csv'; 
+pressureFileName = 'acpr_1.csv';
+[violinInfos, velocityFields, hologramInfos, pressureFields] = importData(velocityFileName, pressureFileName);
+
+pressFieldsDown = cell(size(pressureFields));
+for ii = 1:length(pressureFields)
+    % Downsampling
+    pressFieldsDown{ii} = downsampling(pressureFields{ii}, 8, 8);
+end
+
+figure(5)
+surf(hologramInfos{1}, hologramInfos{2}, abs(pressFieldsDown{1}));
+
+% 
+% 
+% 
+% csvVel = readtable("vel_1.csv");
+% meshcord = table2array(csvVel(:,1:3));
+% %meshcord(:, :, meshcord == NaN) = 0;
+% 
+% %% extract x,y axis values without repetitions 
+% 
+% x = unique(meshcord(:,1));
+% y = unique(meshcord(:, 2));
+% gridX = length(x);
+% gridY = length(y);
+% 
+% %% reconconstruct the plate geometry
+% % reshape into size (16*64) then transpose. because reshape orders the
+% % vector by columns --> ex. try  reshape(1:10,[5,2]) we want
+% % reshape(1:10,[2,5])'   
+% z = meshcord(:,3);
+% platemesh = reshape(z, [gridY, gridX]).';   
+% figure 
+% mesh(platemesh);
+% 
+% %% reconstruct the field
+% csvvelmatrix = table2array(csvVel(:,1:49));
+% 
+% selfreq = 3 + 1;
+% meshfreq = [csvvelmatrix(:,1), csvvelmatrix(:,2), csvvelmatrix(:,selfreq)];
+% 
+% platemesh = reshape(z, [gridY, gridX]).';   
+% 
+% velocityfield = zeros(length(x), length(y));
+% for i = 1:length(x)
+%     for j = 1:length(y)
+%         b = meshfreq((16*(i-1)) + j, :);
+%         velocityfield(i, j) = abs(b(3));
+%     end
+% end        
+% 
+% figure 
+% mesh(velocityfield);
+% 
+% figure
+% %surf(velocityfield);
+% 
+% %% Pressure field
+% csvpress = readtable("acpr_1.csv")
+% 
+% % Reconstruct the field
+% csvpressmatrix = zeros(size(csvpress));
+% 
+% % Insert double in the zero matrix
+% for i = 1 : 1024
+%     for j = 1 : 3
+%         csvpressmatrix(i, j) = table2array(csvpress(i, j));
+%     end
+% end
+% 
+% % Convert cell to double
+% for i = 1 : 1024
+%     for j = 4 : 20 % TO DO, there is a data error at index 21
+%         csvpressmatrix(i, j) =table2array(csvpress(i, j));
+%     end
+% end
+% 
+% % Field
+% selfreq = 3 + 1;
+% meshpress = [csvpressmatrix(:,1), csvpressmatrix(:,2), csvpressmatrix(:,selfreq)];
+% 
+% pressfield = zeros(length(x), length(y));
+% for i = 1:length(x)
+%     for j = 1:length(y)
+%         b = meshpress((16*(i-1)) + j, :);
+%         pressfield(i, j) = abs(b(3));
+%     end
+% end        
+% 
+% figure 
+% surf(pressfield);
+% 
+
---
+++ functions/importData.m
@@ -0,0 +1,70 @@
+function [violinInfos, velocityFields, hologramInfos, pressureFields] = importData(velocityFileName, pressureFileName)
+%IMPORTDATA Summary of this function goes here
+%   Detailed explanation goes here
+
+csvVel = readtable(velocityFileName);
+csvPress = readtable(pressureFileName);
+violinMesh = table2array(csvVel(:,1:3));
+hologramMesh = table2array(csvPress(:,1:3));
+
+%% Reconconstruct the plate geometry
+gridX = length(unique(violinMesh(:,1)));
+gridY = length(unique(violinMesh(:, 2)));
+
+% reshape into size (16*64) then transpose. because reshape orders the
+% vector by columns --> 
+% ex. try  reshape(1:10,[5,2])  what we actuallly want is  reshape(1:10,[2,5])'
+
+X =  reshape(violinMesh(:,1), [gridY, gridX]).'; 
+Y =  reshape(violinMesh(:,2), [gridY, gridX]).'; 
+Z =  reshape(violinMesh(:,3), [gridY, gridX]).';
+figure(1)
+surf(X,Y,Z);
+title('Violin surface');
+zlim([0,100]);
+violinInfos = {X,Y,Z}; 
+
+%% Velocity Fields
+numFreqBins = length(table2array(csvVel(1,:)))-3;
+velsMatrix = table2array( csvVel(:,4:numFreqBins+3) );
+
+velocityFields = cell(numFreqBins,1);
+for ii = 1:numFreqBins
+    velocityFields{ii} = reshape(velsMatrix(:,ii), [gridY, gridX]).';   
+end
+
+figure(2)
+surf(X,Y,abs(velocityFields{1}));
+title('Velocity field surface - f1');
+
+%% Reconstruct the hologram geometry
+
+gridX = length(unique(hologramMesh(:,1)));
+gridY = length(unique(hologramMesh(:,2)));
+
+% reshape into size (16*64) then transpose. because reshape orders the
+% vector by columns --> 
+% ex. try  reshape(1:10,[5,2])  what we actuallly want is  reshape(1:10,[2,5])'
+
+X =  reshape(hologramMesh(:,1), [gridY, gridX]).'; 
+Y =  reshape(hologramMesh(:,2), [gridY, gridX]).'; 
+Z =  reshape(hologramMesh(:,3), [gridY, gridX]).';
+figure(3)
+surf(X,Y,Z);
+hologramInfos = {X,Y,Z}; 
+title('Hologram surface');
+
+%% Pressure Fields
+numFreqBins = length(table2array(csvPress(1,:)))-3;
+pressMatrix = table2array( csvPress(:,4:numFreqBins+3) );
+
+pressureFields = cell(numFreqBins,1);
+for ii = 1:numFreqBins
+    pressureFields{ii} = reshape(pressMatrix(:,ii), [gridY, gridX]).';   
+end
+
+figure(4) 
+surf(X,Y,abs(pressureFields{1}));
+title('Pressure field surface - f1');
+end
+
