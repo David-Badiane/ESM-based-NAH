382eeb2c66a0de3861df7ef8836dc1d15a4544bf
Alessio Lampis <58005469+AlessioLampis@users.noreply.github.com>
2021-04-13 00:02:01 +0200

    small

--- functions/Green_matrix.m
+++ functions/Green_matrix.m
@@ -20,11 +20,11 @@ G_w = zeros(length(r(:, 1)), length(s(:, 1)));  %Green's function init
 
 c = 343; % [m/s] speed of sound
 
-for i = 1 : length(r_m(:, 1)) 
-    for j = 1 : length(a_n(:, 1))      
+for i = 1 : length(r(:, 1)) 
+    for j = 1 : length(s(:, 1))      
         G_w(i, j, k) = (1/4/pi) * exp(-1i * omega/c *... 
-            0.001*norm((r_m(i, :) - a_n(j, :)), 2))... %conversion from [mm] to [m]
-            / 0.001*norm((r_m(i, :) - a_n(j, :)), 2);
+            0.001*norm((r(i, :) - s(j, :)), 2))... %conversion from [mm] to [m]
+            / 0.001*norm((r(i, :) - s(j, :)), 2);
     end
 end
 
