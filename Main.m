7aa873e33a25cd56b843ea20557294a5dec015b5
Alessio Lampis <58005469+AlessioLampis@users.noreply.github.com>
2021-04-13 13:54:04 +0200

    inverse problem finished

--- Main.m
+++ Main.m
@@ -17,17 +17,27 @@ pressureFileName = 'acpr_1.csv';
 % Points on the pressure field (hologram)
 hologramPoints = hologramInfos{4};
 
+% Points on the plate surface
+platePoints = violinInfos{4};
+
 % Virtual sources points
 [virtualPoints, lattice] = getVirtualPoints(violinInfos,hologramPoints);
 
-% Green's matrices in a cell array (for each eigenfrequency)
-[G_w] = Green_matrix(hologramPoints , virtualPoints , eigenFreqz);
+% Green's matrices of the hologram-equivalent sources in a cell array (for each eigenfrequency)
+[G_p] = Green_matrix(hologramPoints , virtualPoints , eigenFreqz);
+
+% Green's matrices of the plate surface - equivalent sources in a cell array (for each eigenfrequency)
+[G_v] = Green_matrix(platePoints , virtualPoints , eigenFreqz);
 
 %% Inverse problem
 
 % choose the mode 
 
 mode = 2;
+
+omega = eigenFreqz{mode};
+
+rho = 1.2; % [Kg/m3] air density 
 
 % measurements of pressure vector
 
@@ -39,11 +49,13 @@ p = abs(p); % the magnitude of pressure is needed
 
 p_n = whiteNoise(p); % add white gaussian noise to the mesurement
 
-G = G_w{mode}; % take the Green's function matrix of the chosen mode
+G = G_p{mode}; % take the Green's function matrix of the chosen mode
 
-x_TSVD = TSVD(G, p_n , 10); % perform the TSVD -> estimate the source strength
+q_TSVD = (1/1i*omega*rho).*TSVD(G, p_n , 10); % perform the TSVD -> estimate the source strength
 
-x_TIK = Tikhonov_SVD(G , p_n , 10);  % perform the Tikhonov SVD -> estimate the source strength
+q_TIK = (1/1i*omega*rho).*Tikhonov_SVD(G , p_n , 10);  % perform the Tikhonov SVD -> estimate the source strength
+
+%% direct problem 
 
 ext_v = velocityFields{mode}; % exact velocity 
 
