6ef83d9076e231822c09998bf1cb590cb9f137e6
Alessio Lampis <58005469+AlessioLampis@users.noreply.github.com>
2021-06-08 21:38:31 +0200

    prova

--- platePoints.m
+++ platePoints.m
@@ -4,7 +4,5 @@ clc
 % IMPORTANT: THIS SCRIPT REQUIRES THE Partial Differential Equation Toolbox.
 %% Import the stl file
 
-figure
-model = createpde(3);
-plateStl = importGeometry(model, 'NAH_ESM_mesh_light v15.stl');
-pdegplot(plateStl)
+
+[TR,fileformat,attributes,solidID] = stlread('NAH_ESM_mesh_light v15.stl')
